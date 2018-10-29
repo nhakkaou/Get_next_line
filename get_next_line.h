ba9fc6a1eefacdc117256b824e2325ca58b029ba
Noreddine Hakkaoui <nhakkaou@e1r8p8.1337.ma>
2018-10-29 11:50:21 +0000

    next_line

---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nhakkaou <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/27 13:05:58 by nhakkaou          #+#    #+#             */
+/*   Updated: 2018/10/28 14:27:08 by nhakkaou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "libft/libft.h"
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# define BUFF_SIZE 1
+
+int get_next_line(int fd, char **line);
+
+#endif
